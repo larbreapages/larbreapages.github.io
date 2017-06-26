f55f5bcb3b5b69d9deff835718bc75e5e8f15a2c
stoneo <stoneo@users.noreply.github.com>
2017-06-26 23:36:42 +0200

    Add sticky nav bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 console.log('Welcome to larbreapages.fr');
+
+window.addEventListener('scroll', () => {
+    const scrollpos = window.scrollY;
+    // console.log(scrollpos);
+
+    if (scrollpos > 725) {
+        document.querySelector('.nav').style.backgroundColor = '#f8f8f8';
+        Array.prototype.map.call(document.querySelectorAll('.nav a'), el => (el.style.color = 'black'));
+    } else {
+        Array.prototype.map.call(document.querySelectorAll('.nav a'), el => (el.style.color = 'inherit'));
+        document.querySelector('.nav').style.backgroundColor = 'inherit';
+    }
+});
