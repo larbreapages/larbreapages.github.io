65914c8d47bfa7542aea87a12430bb8e9f021a9f
ston3o <5039608+ston3o@users.noreply.github.com>
2017-06-29 15:38:21 +0200

    Clean interfaces

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 console.log('Welcome to larbreapages.fr');
 
+const getYPosition = el => el.getBoundingClientRect().top + window.scrollY;
+
 window.addEventListener('scroll', () => {
     const scrollpos = window.scrollY;
-    // console.log(scrollpos);
 
-    if (scrollpos > 725) {
+    if (scrollpos > (getYPosition(document.querySelector('#services')) - document.querySelector('.nav').offsetHeight)) {
         document.querySelector('.nav').style.backgroundColor = '#f8f8f8';
         Array.prototype.map.call(document.querySelectorAll('.nav a'), el => (el.style.color = 'black'));
     } else {
