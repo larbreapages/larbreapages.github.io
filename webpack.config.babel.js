cf3a58bfc5b65223cd1203daa2904707295af8a5
ston3o <5039608+ston3o@users.noreply.github.com>
2017-07-22 13:19:15 +0200

    Refactoring CSS

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -18,7 +18,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.html$/, loader: 'html-loader' },
-            { test: /\.s?css$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader', 'sass-loader'] }) },
+            { test: /\.s?css$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader', 'sass-loader', 'postcss-loader'] }) },
             { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
             {
                 test: /\.(png|ico|jpg|gif|svg|woff2?|eot|otf|ttf)(\?.*)?$/,
